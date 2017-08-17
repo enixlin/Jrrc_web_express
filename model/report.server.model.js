747ec7a477d4b0eebf50a52aa1089a90fe1a5b81
enixlin <enixlin@126.com>
2017-08-17 13:14:36 +0800

    add report.server.model.js implement

--- model/report.server.model.js
+++ model/report.server.model.js
@@ -2,7 +2,20 @@ var express = require('express');
 var util = require('../tools/utils')
 
 var CompositeInterface = new util.Interface.Interface('Composite', ['addChild', 'getChild']);
-util.Interface.ensureImplements(CompositeInterface);
+
+var CompositeImplement = function(name) {
+    this.children = [];
+    this.name = name;
+}
+
+/*  */
+CompositeImplement.prototype.addChild = function(name) {
+
+    }
+    // 取得所有子节点
+CompositeImplement.prototype.getChild = function() {
+    return this.children;
+}
 
 report = {
 
