c4634884855ad968b2715ca0942f38fa6de91692
enixlin <enixlin@126.com>
2017-08-17 20:37:51 +0800

    fixed util.js add class extend method

--- model/report.server.model.js
+++ model/report.server.model.js
@@ -1,21 +1,7 @@
 var express = require('express');
 var util = require('../tools/utils')
 
-var CompositeInterface = new util.Interface.Interface('Composite', ['addChild', 'getChild']);
 
-var CompositeImplement = function(name) {
-    this.children = [];
-    this.name = name;
-}
-
-/*  */
-CompositeImplement.prototype.addChild = function(name) {
-
-    }
-    // 取得所有子节点
-CompositeImplement.prototype.getChild = function() {
-    return this.children;
-}
 
 report = {
 
--- tools/utils.js
+++ tools/utils.js
@@ -156,6 +156,40 @@ var Tools = {
                 }
             }
         }
+    },
+
+
+    /**
+     * 类操作
+     */
+    CLASS: {
+        /* 单继承 */
+        Extend: function(superClass, subClass) {
+            var F = new Function();
+            F.prototype = sup.prototype;
+            sub.prototype = new F();
+            // 还原子类的构造器
+            sub.prototype.constructor = sub;
+            // 保存一下父类的原型对象
+            sub.SuperClass = sup.prototype;
+        },
+
+        /* .多继承 */
+        mix: function() {
+            var i = 1,
+                len = arguments.length,
+                target = arguments[0],
+                arg;
+            for (; i < len; i++) {
+                arg = arguments[i];
+                //遍历被继承对象中的属性
+                for (var property in arg) {
+                    // 将被继承对象中的属性复制到目标对象中
+                    target[property] = arg[property];
+                }
+            }
+            return target;
+        }
     }
 };
 
