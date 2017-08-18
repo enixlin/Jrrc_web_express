48405270f7d6da5719dfb409fed65d90a1a749f8
enixlin <enixlin@126.com>
2017-08-18 14:21:43 +0800

    add report abstract method

--- model/report.server.model.js
+++ model/report.server.model.js
@@ -2,27 +2,6 @@ var express = require("express");
 var util = require("../tools/utils");
 var db = require("../routes/mysql");
 
-// var ReportBase = function(name) {
-//     this.name = name;
-// };
-// ReportBase.prototype.create = function(cb) {
-//     // console.log(db);
-//     var sql = "select * from jrrc_user";
-//     db.query(sql, function(err, result) {
-//         // console.log(result);
-//         util.Array.each(result, function(e) {
-//             // console.log(e.name);
-//             cb(e.name);
-//         });
-//     });
-//     //return this.name;
-// };
-
-// var Report_jsl = function(name) {
-//     this.name = name;
-// };
-
-// util.CLASS.Extend(ReportBase, Report_jsl);
 
 /* 报表抽象工厂 */
 var ReportFactory = function(SubClass, SuperClass) {
@@ -45,9 +24,16 @@ ReportFactory.JSL = function() {
     this.type = "JSL";
 };
 Report_jsl.prototype = {
-    create: function(name, startDay, endDay) {
-        throw new Error("抽象方法不能调用");
+    create: function() {
+        throw new Error("抽象方法create不能调用");
+    },
+    getTasks: function() {
+        throw new Error("抽象方法getTasks不能调用");
+    },
+    getRecords: function() {
+        throw new Error("抽象方法getRecords不能调用");
     }
+
 };
 
 /* 部室国际结算报表实现类 */
